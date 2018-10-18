4a699d7f6d78282f6f08346a94f931417a626a90
Patrick Allreitz <patrickallreitz@Patricks-MacBook-Pro.local>
2018-10-18 22:50:33 +0200

    firebase setup with auth

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1 // London is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
+
+exports.login = functions.https.onRequest((req, res) => {
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Login success</title>
+    </head>
+    <body>
+      Well done...
+    </body>
+  </html>`);
+});
